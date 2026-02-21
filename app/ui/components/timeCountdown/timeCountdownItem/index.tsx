8a81a04307d885c15386370a0e22098e8d7cfdd3
vitorbisterso <vitorbisterso@gmail.com>
2026-02-21 09:49:03 -0300

    fix: Fix timecountdown styles in smaller screens

--- app/ui/components/timeCountdown/timeCountdownItem/index.tsx
+++ app/ui/components/timeCountdown/timeCountdownItem/index.tsx
@@ -8,11 +8,13 @@ export default function TimeCountdownItem({
   text,
 }: TimeCountdownItemProps) {
   return (
-    <div className="flex size-30 flex-col gap-3 rounded-lg shadow-sm">
-      <div className="justify-center p-6 text-center text-6xl text-(--text-color-primary)">
+    <div className="flex size-20 flex-col gap-6 rounded-lg shadow-sm xl:size-30 xl:gap-3">
+      <div className="justify-center p-3 text-center text-3xl text-(--text-color-primary) xl:p-6 xl:text-6xl">
         {timeLeft}
       </div>
-      <p className="text-center text-4xl text-(--text-color-primary)">{text}</p>
+      <p className="text-center text-xl text-(--text-color-primary) xl:text-4xl">
+        {text}
+      </p>
     </div>
   )
 }
